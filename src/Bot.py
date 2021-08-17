38835d8065064189536e28e57fae3ea544138434
AlexHappyCode <arpenacoding@gmail.com>
2021-08-17 06:36:59 +0000

    added imalive on ready

--- src/Bot.py
+++ src/Bot.py
@@ -17,6 +17,9 @@ class Bot(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+        channel = self.get_channel(876926597624655923)
+        await channel.send('im alive!!')
+
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
@@ -32,19 +35,19 @@ class Bot(discord.Client):
 
         # if the incoming message wasnt sent by the bot
         if isNotOurMessage and "swag" in message.content:
-                senderID = message.author.id
+            senderID = message.author.id
 
-                if (senderID in self.profiles):
-                    self.profiles[senderID].strikes += 1        # if we have a profile on this user, edit existing values
-                else:
-                    self.profiles[senderID] = UserProfile()     # else, create a profile for them
+            if (senderID in self.profiles):
+                self.profiles[senderID].strikes += 1        # if we have a profile on this user, edit existing values
+            else:
+                self.profiles[senderID] = UserProfile()     # else, create a profile for them
 
-                swagEmoji = ":moneybag:"
+            swagEmoji = ":moneybag:"
 
-                for i in range(self.profiles[senderID].strikes):
-                    swagEmoji *= 2
+            for i in range(self.profiles[senderID].strikes):
+                swagEmoji *= 2
 
-                await message.channel.send(swagEmoji)
+            await message.channel.send(swagEmoji)
 
     def handleVulgarLanguage(self, message):
         # do database stuff
