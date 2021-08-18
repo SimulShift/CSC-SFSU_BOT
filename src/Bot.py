986df4117dae38a4541749cd4fe188f13907c32d
SaberSams <Stephen.sams@live.com>
2021-08-18 09:42:55 -0700

    Refactored code. added functionality for Database

--- src/Bot.py
+++ src/Bot.py
@@ -1,54 +1,77 @@
+from dotenv import load_dotenv
+from os import getenv
 import discord
 
-from UserProfile import UserProfile
+# if not installed run
+# $pip install "pymongo[srv]"
+from pymongo import MongoClient
 
-class Bot(discord.Client):
-    
-    def __init__(self, token, *args, **kwargs):
+# load .env file
+load_dotenv() 
 
-        # set up stuff
-        super().__init__(*args, **kwargs)   # since Bot is a subclass of discord.Client, we gotta call its parents' ctors, ignore args
+# load the Database
+print("Loading Database")
+db_name = getenv('DB_NAME')
+db_pw = getenv("DB_PW")
+db_url = getenv("DB_URL")
+db_client = MongoClient(f"mongodb+srv://{db_name}:{db_pw}@{db_url}")
+db = db_client.discord
 
-        self.profiles = {} # empty dictionary
+# load the Bot
+print("Loading Bot")
+bot = discord.Client()
+token = getenv("DISCORD_API_TOKEN")
 
-        # start execution
-        self.run(token)
 
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+async def strike(user_id):
+    pass
 
-        channel = self.get_channel(876926597624655923)
-        await channel.send('im alive!!')
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+# run when bot connects to a server 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to discord')
 
-        # the channel the message was sent in
-        activeChannel = message.channel
 
-        await self.swag(message)
-        self.handleVulgarLanguage(message)
+# run on every message
+@bot.event
+async def on_message(message):
+    author_id = message.author.id
 
-        
-    async def swag(self, message):
-        isNotOurMessage = message.author.id != self.user.id
+    # Keep the bot from checking its own messages.
+    if author_id is bot.user.id:
+        return
 
-        # if the incoming message wasnt sent by the bot
-        if isNotOurMessage and "swag" in message.content:
-            senderID = message.author.id
+    # Check if this author has an account
+    account = db.accounts.find_one(author_id)
 
-            if (senderID in self.profiles):
-                self.profiles[senderID].strikes += 1        # if we have a profile on this user, edit existing values
-            else:
-                self.profiles[senderID] = UserProfile()     # else, create a profile for them
+    # If there is no account create one.
+    if account is None:
+        db.accounts.insert_one({
+            "user_id":author_id,
+            "strikes":{},
+            "swag":0,
+        })
+        print(f"Account created for {message.author.name}")
 
-            swagEmoji = ":moneybag:"
+       
+#async def swag(message):
 
-            for i in range(self.profiles[senderID].strikes):
-                swagEmoji *= 2
+    # if the incoming message wasnt sent by the bot
+    #if isNotOurMessage and "swag" in message.content:
+    #    senderID = message.author.id
 
-            await message.channel.send(swagEmoji)
+     #   if (senderID in self.profiles):
+      #      profiles[senderID].strikes += 1        # if we have a profile on this user, edit existing values
+       # else:
+        #    profiles[senderID] = UserProfile()     # else, create a profile for them
 
-    def handleVulgarLanguage(self, message):
-        # do database stuff
-        vulgarLanguage = ['shit', 'fuck']
+    #    swagEmoji = ":moneybag:"
+
+    #    for i in range(self.profiles[senderID].strikes):
+    #        swagEmoji *= 2
+
+    #   await message.channel.send(swagEmoji)
+
+
+bot.run(token)
