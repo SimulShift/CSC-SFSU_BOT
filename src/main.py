7977241c6c9a712e71dc03af66687b46db531fac
AlexHappyCode <arpenacoding@gmail.com>
2021-08-18 19:17:56 +0000

    snatched the old main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Imported Libraries
+from dotenv import load_dotenv
+import os
+
+# Our Libraries
+from Bot import Bot
+
+def main():
+    load_dotenv() # load .env file
+    print('botenv: ', os.getenv('BOTENV'))
+
+    token = os.getenv('TOKEN')
+    bot = Bot(token)
+
+main()
