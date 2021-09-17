b74141655d61cc91b9347e4d9bdd0addf413375d
AlexHappyCode <arpenacoding@gmail.com>
2021-09-17 06:17:40 +0000

    added minimal working bot typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world in typescript')
+import { Client, Intents } from "discord.js"
+import 'dotenv/config' // load .env file
+
+(async () => {
+  const BOT = new Client({ intents: [Intents.FLAGS.GUILDS]})
+
+  await BOT.login(process.env.DISCORD_API_TOKEN)
+})();
