f58e7d391c03d172bf4d4bf7cfd00d3bf7aa598b
SaberSams <Stephen.sams@live.com>
2021-09-17 11:13:38 -0700

    Added new intents
    Added new intents and updated package.json to include prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-import { Client, Intents } from "discord.js"
+import { Client, Intents } from 'discord.js'
 import 'dotenv/config' // load .env file
 
-(async () => {
-  const bot = new Client({ intents: [Intents.FLAGS.GUILDS]})
+;(async () => {
+  /*Creating a new client */
+  const bot = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS, // Create threads, may
+      Intents.FLAGS.GUILD_MESSAGES, // For reading messages and filiting profanity.
+      Intents.FLAGS.GUILD_MESSAGE_TYPING, // To set the bot to tpying when its loading data from a website.
+      Intents.FLAGS.DIRECT_MESSAGES, // To notifiy a user when a message is deleted with an explination.
+    ],
+  })
 
+<<<<<<< Updated upstream
   await bot.login(process.env.DISCORD_API_TOKEN)
+=======
+  bot.once('ready', () => {
+    console.log('Ready!')
+  })
+
+  await bot.login(process.env.DISCORD_API_TOKEN).then(() => {
+    console.log(bot.user?.username + ' has logged in')
+  })
+>>>>>>> Stashed changes
 })()
