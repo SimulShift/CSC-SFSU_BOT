2fab5a1bc6d86d6c32c633dbd0ffdafd2e2aeb7c
AlexHappyCode <arpenacoding@gmail.com>
2021-09-17 16:30:08 +0000

    changed BOT to bot

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Client, Intents } from "discord.js"
 import 'dotenv/config' // load .env file
 
 (async () => {
-  const BOT = new Client({ intents: [Intents.FLAGS.GUILDS]})
+  const bot = new Client({ intents: [Intents.FLAGS.GUILDS]})
 
-  await BOT.login(process.env.DISCORD_API_TOKEN)
+  await bot.login(process.env.DISCORD_API_TOKEN)
 })();
