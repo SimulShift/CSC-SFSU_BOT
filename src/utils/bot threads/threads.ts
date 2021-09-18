07f0257f5f79dbd10df3939e99315a2b2e2d1292
hunter-graves <server.hunter@gmail.com>
2021-09-18 12:46:20 -0700

    poc for thread deletion

--- src/utils/bot threads/threads.ts
+++ src/utils/bot threads/threads.ts
@@ -1,5 +1,5 @@
 import { ThreadAutoArchiveDuration } from 'discord-api-types'
-import { Message } from 'discord.js'
+import { Message, ThreadChannel } from 'discord.js'
 const Discord = require("discord.js")
 export class threads {
     message:Message // must provide a message
@@ -15,8 +15,32 @@ export class threads {
             name : this.name, // give it the name
             autoArchiveDuration : ThreadAutoArchiveDuration.OneDay // time until archived
         }).then(() => {
+            try {
+                if (this.message.thread) {
+                    this.message.thread.ownerId = this.message.author.id
+                }
+            } 
+            catch {
+                console.log("Failed to set owner id:\n", this.message.author.id,"\n")
+            }
             // return the ThreadChannel
             return this.message.thread
         })
+    }
+
+    destroy() :void {
+        var thread: ThreadChannel | null
+        try {
+            thread = this.message.thread
+        }
+        catch {
+            this.message.delete()
+            return
+        }
+        if (thread) {
+            if (thread.ownerId == this.message.author.id) {
+                thread.delete()
+            }
+        }
     }
 }
